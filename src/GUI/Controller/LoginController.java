29afec70c8d2fde5ac68b0a898079ef49683cd59
andersthomsen92 <doomknight92@gmail.com>
2024-03-20 22:18:10 +0100

    Login Pure-GUI has been completed.
    Created CSS and Images Folder in Resources dir.

--- src/GUI/Controller/LoginController.java
+++ src/GUI/Controller/LoginController.java
@@ -1,4 +1,25 @@
 package GUI.Controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private MFXButton btnForgot;
+
+    @FXML
+    private Label lblPass;
+
+    @FXML
+    private Label lblUser;
+
+    @FXML
+    private TextField txtPass;
+
+    @FXML
+    private TextField txtUser;
+
 }
--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("EventWindow.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root);
