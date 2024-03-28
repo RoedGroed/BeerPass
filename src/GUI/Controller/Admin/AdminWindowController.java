09ef5436ec8aadcd13e008b7e1c24f4b67329608
Autz3n <madsautzen@gmail.com>
2024-03-28 11:17:39 +0100

    Admin window done

--- src/GUI/Controller/Admin/AdminWindowController.java
+++ src/GUI/Controller/Admin/AdminWindowController.java
@@ -1,4 +1,23 @@
 package GUI.Controller.Admin;
 
+import javafx.event.ActionEvent;
+
 public class AdminWindowController {
+    public void onLogout(ActionEvent actionEvent) {
+    }
+
+    public void onEvent(ActionEvent actionEvent) {
+    }
+
+    public void onAdmin(ActionEvent actionEvent) {
+    }
+
+    public void onTicket(ActionEvent actionEvent) {
+    }
+
+    public void onEditUser(ActionEvent actionEvent) {
+    }
+
+    public void onNewUser(ActionEvent actionEvent) {
+    }
 }
