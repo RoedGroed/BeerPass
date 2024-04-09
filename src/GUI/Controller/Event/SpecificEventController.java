08bdba8a6e0bee56cbd96b9c1cdab7ae63704beb
Jonas Roer <jonroe01@easv365.dk>
2024-04-09 22:02:22 +0200

    midlertidigt push

--- src/GUI/Controller/Event/SpecificEventController.java
+++ src/GUI/Controller/Event/SpecificEventController.java
@@ -1,6 +1,7 @@
 package GUI.Controller.Event;
 
 import BE.Event;
+import BE.Ticket;
 import BE.User;
 import GUI.Controller.BaseController;
 import GUI.Model.EventModel;
@@ -20,6 +21,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class SpecificEventController extends BaseController implements Initializable {
@@ -46,10 +48,74 @@ public class SpecificEventController extends BaseController implements Initializ
     private TextField tfSearch;
     private EventModel eventModel;
     private Event event;
+    @FXML
+    private ListView lvRadioBtns;
+    
+    @FXML
+    private ToggleGroup grpSelectTicket;
+    @FXML
+    private ToggleButton tBtnSpecial;
+    @FXML
+    private ToggleButton tBtnEvent;
+    @FXML
+    private Label lblSeleTicket;
+    @FXML
+    private Label lblSeleUser;
 
     public void initialize(URL location, ResourceBundle resources){
         super.initialize(location, resources);
         eventModel = new EventModel();
+
+
+
+
+        tBtnSpecial.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            updateVisibility(newValue, false);
+        });
+
+        tBtnEvent.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            updateVisibility(false, newValue);
+        });
+    }
+
+    private void updateVisibility(boolean specialSelected, boolean eventSelected) {
+        lblSeleUser.setVisible(eventSelected);
+        tfSearch.setVisible(eventSelected);
+        lvAllUsers.setVisible(eventSelected);
+        lblSeleTicket.setVisible(specialSelected || eventSelected);
+        lvRadioBtns.setVisible(specialSelected || eventSelected);
+
+        if (eventSelected) {
+            //populateRadioButtonsForEventTickets();
+        } else if (specialSelected) {
+            populateRadioButtonsForSpecialTickets();
+        }
+
+    }
+
+    private void populateRadioButtonsForEventTickets() {
+        /*private void populateRadioButtonsForEventTickets(int eventId) {
+            List<Ticket> ticketsForEvent = ticketModel.getTicketsForEvent(eventId);
+            lvRadioBtns.getItems().clear();
+
+            ToggleGroup ticketToggleGroup = new ToggleGroup();
+
+            for (Ticket ticket : ticketsForEvent) {
+                RadioButton radioButton = new RadioButton(ticket.getName());
+                radioButton.setUserData(ticket);
+                radioButton.setToggleGroup(ticketToggleGroup);
+
+                lvRadioBtns.getItems().add(radioButton);
+            }*/
+
+        }
+
+        private void updateTicketPreview(Ticket ticket) {
+
+        }
+
+
+    private void populateRadioButtonsForSpecialTickets() {
     }
 
     @FXML
@@ -115,13 +181,25 @@ public class SpecificEventController extends BaseController implements Initializ
     public void populateFields(Event event){
         this.event = event;
 
+
         lblEventName.setText(event.getName());
         lblInfo.setText(event.getTime() + " || " + event.getLocation());
         lblTicketCounter.setText(String.valueOf(event.getTicketLimit()));
         //FIXME: Der skal laves en måde at regne de solgte billeter ud på.
         taEventNotes.setText(event.getNote());
+        //populateTickets(event);
 
     }
+
+    /*private void populateTickets(Event event) {
+        List<Ticket> ticketsForEvent = ticketModel.getTicketsForEvent(event.getEventID());
+        for (Ticket ticket : ticketsForEvent) {
+            RadioButton radioButton = new RadioButton(ticket.getTicketName());
+            radioButton.setToggleGroup(ticketToggleGroup);
+            radioButton.setUserData(ticket);
+            lvRadioBtns.getItems().add(radioButton);
+        }
+    }*/
 
 
 }
