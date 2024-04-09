d114f29ae263d6ea720fadf14962ac7be9145bd0
Pumpozo <adnriz01@easv365.dk>
2024-04-09 13:46:42 +0200

    Ticket to be made

---
+++ src/GUI/Controller/Ticket/TicketController.java
@@ -0,0 +1,25 @@
+package GUI.Controller.Ticket;
+
+
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+
+public class TicketController {
+
+    @FXML
+    private Label eventName;
+    @FXML
+    private Label eventLocation;
+    @FXML
+    private Label eventTime;
+    @FXML
+    private Label eventNotes;
+    @FXML
+    private Label costumerName;
+    @FXML
+    private ImageView barcode;
+    @FXML
+    private ImageView qrCode;
+}
